5dc169b392bc652713d0c38104db70a509f506d5
EdwardSummitt <eddiesummitt1645@gmail.com>
2025-11-11 10:58:05 -0600

    Update comments to remove documentation syntax

--- csc-4101-fall24-project-i-calcpl-proj-template-main/src/main.ml
+++ csc-4101-fall24-project-i-calcpl-proj-template-main/src/main.ml
@@ -1,41 +1,41 @@
 open Ast
 (*applebees*)
-(** [parse s] parses [s] into an AST. *)
+(* [parse s] parses [s] into an AST. *)
 let parse (s : string) : expr =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
   ast
 
-(** The error message produced if a variable is unbound. *)
+(* The error message produced if a variable is unbound. *)
 let unbound_var_err = "Unbound variable"
 
-(** The error message produced if binary operators and their
+(* The error message produced if binary operators and their
     operands do not have the correct types. *)
 let bop_err = "Operator and operand type mismatch"
 
-(** The error message produced if the [then] and [else] branches
+(* The error message produced if the [then] and [else] branches
     of an [if] do not have the same type. *)
 let if_branch_err = "Branches of if must have same type"
 
-(** The error message produced if the guard
+(* The error message produced if the guard
     of an [if] does not have type [bool]. *)
 let if_guard_err = "Guard of if must have type bool"
 
-(** The error message produced if type mismatches on let. *)
+(* The error message produced if type mismatches on let. *)
 let annotation_err = "Let expression type mismatch"
 
 module type StaticEnvironment = sig
-  (** [t] is the type of a static environment. *)
+  (* [t] is the type of a static environment. *)
   type t
 
-  (** [empty] is the empty static environment. *)
+  (* [empty] is the empty static environment. *)
   val empty : t
 
-  (** [lookup env x] gets the binding of [x] in [env].
+  (* [lookup env x] gets the binding of [x] in [env].
       Raises: [Failure] if [x] is not bound in [env]. *)
   val lookup : t -> string -> typ
 
-  (** [extend env x ty] is [env] extended with a binding
+  (* [extend env x ty] is [env] extended with a binding
       of [x] to [ty]. *)
   val extend : t -> string -> typ -> t
 end
@@ -55,13 +55,13 @@ end
 
 open StaticEnvironment
 
-(** Converts x float to d point float ex. round_dfrac 2 3.140000009 returns 3.14*)
+(* Converts x float to d point float ex. round_dfrac 2 3.140000009 returns 3.14*)
 let round_dfrac d x =
   if x -. (Float.round x) = 0. then x else                   (* x is an integer. *)
   let m = 10. ** (float d) in                       (* m moves 10^-d to 1. *)
   (floor ((x *. m) +. 0.5)) /. m
 
-(** [typeof env e] is the type of [e] in context [env]. 
+(* [typeof env e] is the type of [e] in context [env]. 
     Raises: [Failure] if [e] is not well typed in [env]. *)
 let rec typeof env = function
   | Int _ -> TInt
@@ -72,7 +72,7 @@ let rec typeof env = function
   | If (e1, e2, e3) -> typeof_if env e1 e2 e3
   | _ -> failwith "TODO"
   
-(** Helper function for [typeof]. *)
+(* Helper function for [typeof]. *)
 and typeof_let env x t e1 e2 = 
   let t' = typeof env e1 in
   if t = t' then
@@ -81,7 +81,7 @@ and typeof_let env x t e1 e2 =
   else 
       failwith annotation_err
   
-(** Helper function for [typeof]. *)
+(* Helper function for [typeof]. *)
 and typeof_bop env bop e1 e2 =
   let t1, t2 = typeof env e1, typeof env e2 in
   match bop, t1, t2 with
@@ -90,7 +90,7 @@ and typeof_bop env bop e1 e2 =
   | Leq, TInt, TInt -> TBool
   | _ -> failwith bop_err
   
-(** Helper function for [typeof]. *)
+(* Helper function for [typeof]. *)
 and typeof_if env e1 e2 e3 =
   let t1 = typeof env e1 in 
   if t1 <> TBool then
@@ -103,12 +103,12 @@ and typeof_if env e1 e2 e3 =
     else 
       t2
 
-(** [typecheck e] checks whether [e] is well typed in
+(* [typecheck e] checks whether [e] is well typed in
   the empty environment. Raises: [Failure] if not. *)
 let typecheck e =
   ignore (typeof empty e)
 
-(** [subst e v x] is [e] with [v] substituted for [x], that
+(* [subst e v x] is [e] with [v] substituted for [x], that
     is, [e{v/x}]. *)
 let rec subst e v x = match e with
   | Var y -> if x = y then v else e
@@ -124,7 +124,7 @@ let rec subst e v x = match e with
     If (subst e1 v x, subst e2 v x, subst e3 v x)
   | _ -> failwith "TODO"
   
-(** [eval e] the [v]such that [e ==> v]. *)
+(* [eval e] the [v]such that [e ==> v]. *)
 let rec eval (e : expr) : expr = match e with
   | Int _ | Bool _ -> e
   | Var _ -> failwith unbound_var_err
@@ -133,13 +133,13 @@ let rec eval (e : expr) : expr = match e with
   | If (e1, e2, e3) -> eval_if e1 e2 e3
   | _ -> failwith "TODO"
 
-(** [eval_let x e1 e2] is the [v] such that [let x = e1 in e2 ==> v]. *) 
+(* [eval_let x e1 e2] is the [v] such that [let x = e1 in e2 ==> v]. *) 
 and eval_let x e1 e2 = 
   let v1 = eval e1 in 
   let e2' = subst e2 v1 x in 
   eval e2'
 
-(** [eval_bop bop e1 e2] is the [v] such that [e1 bop e2 ==> v]. *) 
+(* [eval_bop bop e1 e2] is the [v] such that [e1 bop e2 ==> v]. *) 
 and eval_bop bop e1 e2 = 
   match bop, eval e1, eval e2 with
   | Add, Int a, Int b -> Int (a + b)
@@ -147,14 +147,14 @@ and eval_bop bop e1 e2 =
   | Leq , Int a, Int b -> Bool (a <= b)
   | _ -> failwith bop_err
 
-(** [eval_if e1 e2 e3] is the [v] such that [if e1 then e2 ==> v]. *) 
+(* [eval_if e1 e2 e3] is the [v] such that [if e1 then e2 ==> v]. *) 
 and eval_if e1 e2 e3 = 
   match eval e1 with 
   | Bool true -> eval e2
   | Bool false -> eval e3
   | _ -> failwith if_guard_err
 
-(** [interp s] interprets [s] by lexing and parsing,
+(* [interp s] interprets [s] by lexing and parsing,
     evaluating, and converting the result to string. *)
 let interp (s : string) : expr =
   let e = parse s in
